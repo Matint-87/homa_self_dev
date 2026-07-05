cbb2cebc5a15f7d6de36dd468ad242bfc29eff66
matin <sajjad.torabi95@gmail.com>
2026-07-05 22:54:37 +0330

    sfd

--- handlers/ping.py
+++ handlers/ping.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timezone
 from telethon import events
-from decorators import require_balance
 
-@require_balance
 async def ping_handler(event):
     try:
         me = await event.client.get_me()
